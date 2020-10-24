e4dccfc01796520353a77cc2397381adcc55aed8
Owen Conti <owen@ohseemedia.com>
2020-10-24 12:56:17 -0600

    fix: Fix phpcs

--- tests/Fixtures/HasAnalyticsModel.php
+++ tests/Fixtures/HasAnalyticsModel.php
@@ -5,7 +5,8 @@ namespace OhSeeSoftware\LaravelServerAnalytics\Tests\Fixtures;
 use OhSeeSoftware\LaravelServerAnalytics\Models\Analytics;
 use OhSeeSoftware\LaravelServerAnalytics\Traits\HasAnalytics;
 
-class HasAnalyticsModel extends Analytics {
+class HasAnalyticsModel extends Analytics
+{
     use HasAnalytics;
 
     public $table = 'analytics';
